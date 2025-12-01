193c696db44ab750aa90187d9b42e2a4aa9c6cb4
Toofy Admin <admin@toofy.com>
2025-12-02 00:49:03 +0300

    fix: export Direction type from direction-provider

--- frontend/src/context/direction-provider.tsx
+++ frontend/src/context/direction-provider.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react'
 
-type Direction = 'ltr' | 'rtl'
+export type Direction = 'ltr' | 'rtl'
 
 type DirectionContextType = {
   direction: Direction
